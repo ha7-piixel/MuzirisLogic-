95e5360c1844f345ff2a099247e7dad271e39791
ha7-piixel <harikrishnanm430@gmail.com>
2025-12-27 18:26:42 +0530

    The python code for the ML project of Entity Linking is done here

---
+++ wikipedia_knowledge_base.py
@@ -0,0 +1,18 @@
+#Importing the wikipedia library 
+
+def curate_date(data_list):
+    curated_results = {}
+    for item in data_list:
+        try:
+            #Get the page title for the search term 
+            page = wikipedia.page(item)
+            curated_results[item] = page.url
+            print(f"Linked: {item} -> {page.url}")
+        except:
+            #Handled cases where Wikipedia is confused or the item isn't found
+            curated_results[item] = "No Wikipedia link is found"
+    return curated_results
+#Your "Dataset"
+my_data = ["Ebenezer Scrooge", "Machine Learning", "Git Bash"]
+link = curate_data(my_data)
+
