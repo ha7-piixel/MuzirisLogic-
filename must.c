9b1d3f1c8d3be4e43de37bad7ca8e6824cce66b6
ha7-piixel <harikrishnanm430@gmail.com>
2025-12-27 18:01:29 +0530

    Added the first C program

---
+++ must.c
@@ -0,0 +1,16 @@
+//This is a new C program for to understand the differences between the negative and the positive numbers 
+#include <stdio.h>
+void posneg(int a){
+	if(a > 0)
+		printf("The given number %d is a positive number", a);
+	if(a == 0)
+		printf("The given number is a 0");
+	else 
+		printf("The given number %d is a negative number", a);
+}
+int main(){
+	int n;
+	printf("Enter the number you want to simulate: ");
+	scanf("%d", &n);
+	posneg(n);
+}
